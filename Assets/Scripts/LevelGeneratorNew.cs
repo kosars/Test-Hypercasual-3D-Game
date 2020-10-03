3cd7739e3df6d7e4a9f0c19915b4b5b9f196579f
kosars <kosarsMK@gmail.com>
2020-10-03 13:58:54 +0300

    Code Refactoring (Level Generator)

---
+++ Assets/Scripts/LevelGeneratorNew.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneratorNew : MonoBehaviour
+{
+    [SerializeField] private LevelData _currentLevel;
+    [SerializeField] private GameObject _levelCompleteTrigger;
+    [SerializeField] private Transform _levelTransform;
+
+    private float _spawnHight = 2f;//TODO: MAKE FROM GAME SETTINGS ScriptableObject
+
+    private float _lastSpawnPosition;
+
+
+    private void Awake()
+    {
+        //TODO: GET DATA FORM LEVEL
+        GameObject[] gameObjects = _currentLevel.GameObjects;
+        List<ObjectsRow> levelObjects = _currentLevel.LevelObjects;
+        //TODO: SPAWN OBJECTS
+        GenerateLevel(levelObjects, gameObjects);
+        //TODO: SET THE TARGET SCORE
+        PlayerScore.TargetScore = _currentLevel.TargetScore;
+
+    }
+
+    private void GenerateLevel(List<ObjectsRow> levelObjects, GameObject[] gameObjects)
+    {
+        if (null == levelObjects 
+            || null == gameObjects)
+            return;
+        foreach(ObjectsRow objectsRow in levelObjects)
+        {
+            SpawnRow(objectsRow, gameObjects);
+        }
+
+        SpawnTrigger(_levelCompleteTrigger, _lastSpawnPosition);
+    }
+
+    private void SpawnObject(GameObject objectPrefab, float position, float rotation)
+    {
+        Vector3 pos = new Vector3(0.0f, _spawnHight, position);
+        Quaternion rot = Quaternion.Euler(Vector3.forward);
+        Transform t = Instantiate(objectPrefab, pos, rot).transform;
+        RotateObject(t, rotation);
+        t.SetParent(_levelTransform);
+    }
+
+    private void SpawnRow(ObjectsRow objectsRow, GameObject[] gameObjects)
+    {
+        float position = _lastSpawnPosition + objectsRow.StartOffset;
+        float rotation = objectsRow.StartAngle;
+
+        for (int i=0; i<objectsRow.Amount; i++)
+        { 
+            SpawnObject(
+                gameObjects[objectsRow.Index],
+                position,
+                rotation);
+            position += objectsRow.BeetweenOffset;
+            rotation += objectsRow.BeetweenAngle;
+        }
+
+        _lastSpawnPosition = position - objectsRow.BeetweenOffset;
+    }
+    
+    private void SpawnTrigger(GameObject obj, float zPosition)
+    {
+        Vector3 position = new Vector3(0.0f, 0.0f, zPosition);
+        Quaternion rotation = Quaternion.Euler(Vector3.forward);
+        Instantiate(obj, position, rotation);
+    }
+
+    private void RotateObject(Transform transform, float rotateAngle) => transform.RotateAround(Vector3.zero, Vector3.forward, rotateAngle);
+}
---
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/LevelData", fileName = "New Level")]
+public class LevelData : ScriptableObject
+{
+    [SerializeField] private int _number;
+    [SerializeField] private int _targetScore;
+    [SerializeField] private GameObjectsList _gameObjects;
+    [SerializeField] private List<ObjectsRow> _levelObjects;
+
+
+    public int Number { get => _number; }
+    public string Name { get => "Level " + _number.ToString(); }
+    public int TargetScore { get => _targetScore; }
+    public List<ObjectsRow> LevelObjects { get => _levelObjects; }
+    public GameObject[] GameObjects { get => _gameObjects.GetObjects; }
+
+}
+
+[System.Serializable]
+public class ObjectsRow
+{
+    [SerializeField] private int _gameObjectsIndex;
+    [SerializeField] private int _levelObjectsAmount = 1;
+    [SerializeField] private float _rotationStartAngle = 0f; //start Angle rotation in row
+    [SerializeField] private float _rotationBeetweenAngle = 0f; //rotation angle beetwen objects
+    [SerializeField] private float _startOffset = 10f;
+    [SerializeField] private float _beetweenOffset = 1f; //offset beetwen 
+
+    public int Index { get => _gameObjectsIndex; }
+    public int Amount { get => _levelObjectsAmount; }
+    public float StartAngle { get => _rotationStartAngle; }
+    public float BeetweenAngle { get => _rotationBeetweenAngle; }
+    public float StartOffset { get => _startOffset; }
+    public float BeetweenOffset { get => _beetweenOffset; }
+
+
+
+}
